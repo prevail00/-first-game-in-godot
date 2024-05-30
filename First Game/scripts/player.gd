f35c79d643ef1f67e58d59e0148ff5db33b337f3
prevail00 <113232108+prevail00@users.noreply.github.com>
2024-05-30 21:50:58 +0200

    Add enemy, enemy movement, player death features
    Created an enemy that is moving back and forth.
    Made the map bigger and painted some backgrund

--- First Game/scripts/player.gd
+++ First Game/scripts/player.gd
@@ -1,26 +1,40 @@
 extends CharacterBody2D
 
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
-var jump_count = 2
+const SPEED = 110.0
+const JUMP_VELOCITY = -250.0
+var jump_count = 0
+var max_jump_count = 2
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Handle double jump
+	if jump_count > 0 and is_on_floor(): #Resets double jump on landing
+		jump_count = 0
+		print("Jump count reset")
+	if Input.is_action_just_pressed("jump") and jump_count < max_jump_count:
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+		jump_count += 1
+		print(jump_count)
+	
+	# Get the input direction: -1, 0, 1
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	#Flip the Sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+	#Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
