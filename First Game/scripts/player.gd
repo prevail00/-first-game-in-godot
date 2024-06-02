6595ae1a2e3569ce7dd840ddc30c9a71454db738
prevail00 <113232108+prevail00@users.noreply.github.com>
2024-06-02 19:23:36 +0200

    Add sounds and jump animation
    Added background music. Sounds for jumping and coin pickup. Made a flip animation for double jump

--- First Game/scripts/player.gd
+++ First Game/scripts/player.gd
@@ -10,11 +10,20 @@ var max_jump_count = 2
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var animation_player = $AnimationPlayer
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	# Get the input direction: -1, 0, 1
+	var direction = Input.get_axis("move_left", "move_right")
+	#Play animation
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
 
 	# Handle double jump
 	if jump_count > 0 and is_on_floor(): #Resets double jump on landing
@@ -23,17 +32,22 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and jump_count < max_jump_count:
 		velocity.y = JUMP_VELOCITY
 		jump_count += 1
-		print(jump_count)
+		animation_player.play("jump")
+		if jump_count == 1:
+			animated_sprite.play("jump")
+			print(jump_count)
+		elif jump_count > 1:
+			animated_sprite.play("double_jump")
+			print(jump_count)
 	
-	# Get the input direction: -1, 0, 1
-	var direction = Input.get_axis("move_left", "move_right")
+
 	
 	#Flip the Sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-		
+	
 	#Apply movement
 	if direction:
 		velocity.x = direction * SPEED
